a8bd570cdd5e4c38f831b0dcbd7128d748f64ca8
marek <saltinio@gmail.com>
2018-01-24 17:46:18 +0200

    Numbers added.

--- DesktopCalculator/DesktopCalculator/MainWindow.xaml.cs
+++ DesktopCalculator/DesktopCalculator/MainWindow.xaml.cs
@@ -20,45 +20,18 @@ namespace DesktopCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int sum = 0;
+
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void Num1Btn_Click(object sender, RoutedEventArgs e)
         {
-
+            resultBox.Text = resultBox.Text + ((Button)sender).Content;
         }
 
-        private void executeButton_Click(object sender, RoutedEventArgs e)
-        {
-            //result.Content = num1.Text;
-            //int inputNumber = int.Parse(num1.Text);
-            //int squareNumber = inputNumber * inputNumber;
-            //result.Content = squareNumber;
-            result.Content = sum;
-        }
 
-        private void clearResult_Click(object sender, RoutedEventArgs e)
-        {
-            num1.Text = String.Empty;
-            result.Content = "";
-            sum = 0;
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
- 
-        }
-
-        private void plus_Click(object sender, RoutedEventArgs e)
-        {
-            int inputNumber = int.Parse(num1.Text);
-            result.Content = inputNumber;
-            sum = sum + inputNumber;
-            num1.Text = String.Empty;
-        }
     }
 }
