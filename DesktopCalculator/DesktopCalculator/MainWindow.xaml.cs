179ff21a5bf1cdbddf1c25b167c0ca9666c8007c
marek <saltinio@gmail.com>
2018-01-29 19:41:22 +0200

    Optimized + and -. One method for both.

--- DesktopCalculator/DesktopCalculator/MainWindow.xaml.cs
+++ DesktopCalculator/DesktopCalculator/MainWindow.xaml.cs
@@ -30,19 +30,13 @@ namespace DesktopCalculator
 
         private void plusBtn_Click(object sender, RoutedEventArgs e)
         {
-            resultBox.Text = resultBox.Text + "+";
+            string buttonValue = ((Button)sender).Content.ToString();
+            resultBox.Text = resultBox.Text + ((Button)sender).Content;
             part2 = part1;
             part1 = "";
-            action = "+";
+            action = buttonValue;
         }
 
-        private void minusBtn_Click(object sender, RoutedEventArgs e)
-        {
-            resultBox.Text = resultBox.Text + "-";
-            part2 = part1;
-            part1 = "";
-            action = "-";
-        }
 
         private void executeBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -51,12 +45,13 @@ namespace DesktopCalculator
             if (action == "+")
             {
                 resultBox.Text = (partOne + partTwo).ToString();
-            } else if (action == "-")
+            }
+            else if (action == "-")
             {
                 resultBox.Text = (partTwo - partOne).ToString();
             }
 
-           part1 = resultBox.Text;
+            part1 = resultBox.Text;
         }
 
 
