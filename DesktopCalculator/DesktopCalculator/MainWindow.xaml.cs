6db31173bbd483913bf19950fc90f53ab448ddb7
marek <saltinio@gmail.com>
2018-01-29 18:51:24 +0200

    Removed some trash.

--- DesktopCalculator/DesktopCalculator/MainWindow.xaml.cs
+++ DesktopCalculator/DesktopCalculator/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace DesktopCalculator
 {
@@ -37,10 +26,7 @@ namespace DesktopCalculator
             part1 = part1 + buttonValue;
         }
 
-        private void resultBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
 
-        }
 
         private void plusBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -72,5 +58,7 @@ namespace DesktopCalculator
 
            part1 = resultBox.Text;
         }
+
+
     }
 }
